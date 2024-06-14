d6208a24c932d7c5c9237cb7b52a065f34cf2597
dishan1223 <dishanishtiaq45+@gmail.com>
2024-06-14 16:11:10 +0600

    added task removal and task completion feature

--- todolist/main.js
+++ todolist/main.js
@@ -10,10 +10,22 @@ document.addEventListener('DOMContentLoaded', () => {
       let li = document.createElement('li');
       li.textContent = inputBox.value;
       listContainer.appendChild(li);
-      inputBox.value = ""; 
+      let span = document.createElement('span');
+      span.innerHTML = '\u00d7';
+      li.appendChild(span);
     }
+    inputBox.value = ""; 
   }
 
   addButton.addEventListener('click', addTask);
+
+  listContainer.addEventListener('click', function(e){
+    if(e.target.tagName == 'LI'){
+      e.target.classList.toggle('checked');
+    }
+    else if(e.target.tagName === "SPAN"){
+      e.target.parentElement.remove();
+    }
+  }, false)
 });
 
