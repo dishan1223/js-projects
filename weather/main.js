92c559c385e3f2af5d66c8bccf26983756752836
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-06-16 17:09:24 +0600

    added API support to fetch weather info

--- weather/main.js
+++ weather/main.js
@@ -1,11 +1,21 @@
-const apiKey = 'Api Key';
+const apiKey = 'Your Api key';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric';
+const inputBox = document.getElementById('input-box');
+const searchBtn = document.getElementById('search');
 
-async function checkWeather()
+async function checkWeather(city)
 {
-  const response = await fetch(apiUrl + `&q=dhaka`  + `&appid=${apiKey}`);
+  const response = await fetch(apiUrl + `&q=${city}`  + `&appid=${apiKey}`);
   var data = await response.json();
   console.log(data);
+
+  document.querySelector('.location').textContent = data.name;
+  document.querySelector('.temperature').textContent = Math.floor(data.main.temp)+'°C';
+  document.querySelector('.wind').textContent = data.wind.speed + " km/h";
+  document.querySelector('.humidity').textContent = data.main.humidity + "%";
 }
 
-checkWeather();
+searchBtn.addEventListener('click', () => {
+  checkWeather(inputBox.value);
+})
+
