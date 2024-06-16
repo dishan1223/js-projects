bb8a0cdac1da7968e6aedf503416ebbb5a88d40c
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-06-16 21:29:57 +0600

    v1

--- narutodb/main.js
+++ narutodb/main.js
@@ -1,16 +1,24 @@
-const apiUrl = 'https://narutodb.xyz/api/character/search?name=naruto%20uzumaki';
+const apiUrl = 'https://narutodb.xyz/api/character/search?name=';
 const charImg = document.querySelector(".char");
+const inputBox = document.getElementById('input-box');
+const btn = document.getElementById("search-btn");
 
-async function searchNarutoDB()
+async function searchNarutoDB(charName)
 {
-  charImg.src = 'images/default-img.jpg'
-  const response = await fetch(apiUrl);
+
+  const response = await fetch(apiUrl + charName);
   var data = await response.json();
   console.log(data);
   document.querySelector('.name').textContent = data.name;
   document.querySelector('.clan').innerHTML ='Clan Name: '+ data.personal.clan;
+  charImg.src = `${data.images[0]}`;
+  document.querySelector('.jutsu').innerHTML = 'Main Jutsu: ' + data.jutsu[0];
+  document.querySelector('.nature').innerHTML = 'Nature: ' + data.natureType[0];
+
 }
 
+btn.addEventListener('click',() => {
+  searchNarutoDB(inputBox.value);
+})
 
-searchNarutoDB();
 
