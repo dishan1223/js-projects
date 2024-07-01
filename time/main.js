4644dd4d5205c94dd02ed9176a657360fe8e7267
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-07-01 10:27:33 +0600

    added time with javascript

--- time/main.js
+++ time/main.js
@@ -1,12 +1,28 @@
-function ResizeClockFont(){
+function ResizeClockFont() {
   let screenWidth = window.innerWidth; // Use window.innerWidth for dynamic resizing
-  let fontSize =  (screenWidth * 260) / 1825;
+  let fontSize = (screenWidth * 260) / 1825;
   let timeFont = document.querySelector('.time');
   if (timeFont) { // Check if the element exists
     timeFont.style.fontSize = fontSize + 'px';
   }
 }
 
-window.onload = ResizeClockFont;
-window.onresize = ResizeClockFont; // Adjust font size on window resize
+
+function getTime(){
+  let time = new Date();
+  let hours = String(time.getHours()).padStart(2, '0'); // Pad with zero if necessary
+  let min = String(time.getMinutes()).padStart(2, '0'); // Pad with zero if necessary
+  let sec = String(time.getSeconds()).padStart(2, '0');
+  let currentTime = `${hours}:${min}:${sec}`;
+  const timer = document.getElementById('time');
+  if(timer){
+    timer.textContent = currentTime;
+  }
+}
+
+window.onload = function(){
+  ResizeClockFont();
+  getTime();
+  setInterval(getTime, 1000);
+}
 
