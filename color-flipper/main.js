83898822599c4e222f00ca2e91b8160209d11e44
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-07-03 13:42:36 +0600

    added flip color button functionality

--- color-flipper/main.js
+++ color-flipper/main.js
@@ -1,11 +1,22 @@
 let backgrnd = document.querySelector('.app');
-
 let changeColor = document.getElementById('change-color');
+
+
+function isValidHEX(hex){
+  const hexRegex = /^#([0-9A-Fa-f]{6})$/;
+  return hexRegex.test(hex);
+}
 
 function flipColor(){
   let input = document.getElementById('hexinput').value;
-  let colorHEX = input;
-  backgrnd.style.background = colorHEX; 
+  
+  if(isValidHEX(input)){
+    let colorHEX = input;
+    backgrnd.style.background = colorHEX;
+  }
+  else{
+    alert("Please enter a valie hex value")
+  }
 }
 
 
