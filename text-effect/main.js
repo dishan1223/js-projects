d4f06b0b3a9d1ab9bd5123904a62d664a96a8cc3
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-07-04 17:24:49 +0600

    text

---
+++ text-effect/main.js
@@ -0,0 +1,19 @@
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+document.querySelector('h1').onmousemove = event => {
+  let iterations = 0;
+
+  const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split('').map((letter, index) => {
+      if(index< iterations){
+        return event.target.dataset.value[index];
+      }
+    
+      return letters[Math.floor(Math.random()* 26)];
+    }).join('');
+    
+    if(iterations >= 5) clearInterval(interval);
+
+    iterations += 1;
+  }, 30);  
+}
