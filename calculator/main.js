148c3728072e9c376c40e29be3dee0ed1de70338
dishan1223 <dishanishtiaq45+github@gmail.com>
2024-07-07 16:17:12 +0600

    some design changes

--- calculator/main.js
+++ calculator/main.js
@@ -4,7 +4,15 @@ function appendToDisplay(char){
   display.value += char;
 }
 
-
 function clearDisplay(){
   display.value = '';
 }
+
+function calculate(){
+  try{
+    display.value = eval(display.value);
+  } catch(error) {
+    alert('Invalid Expression');
+  }
+}
+
